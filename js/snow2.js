ff8b5399f07abafc292b6394c222c95d0b371969
zhuxiaowei0924 <1058386397@qq.com>
2020-12-11 21:05:47 +0800

    Site updated: 2020-12-11 21:05:46

--- js/snow2.js
+++ js/snow2.js
@@ -12,7 +12,7 @@
                 minSize: 10,
                 maxSize: 20,
                 newOn: 1000,
-                flakeColor: "#44cef6" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+                flakeColor: "#D7DBDD" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
             },
             options = $.extend({}, defaults, options);
         var interval = setInterval(function () {
@@ -39,10 +39,10 @@
 })(jQuery);
 $(function () {
     $.fn.snow({
-        minSize: 10,
+        minSize: 3,
         /* 定义雪花最小尺寸 */
-        maxSize: 20,
+        maxSize: 7,
         /* 定义雪花最大尺寸 */
-        newOn: 200 /* 定义密集程度，数字越小越密集 */
+        newOn: 350 /* 定义密集程度，数字越小越密集 */
     });
 });
